2bba12d85aa2c412cf158f7e802ba5dd8a94d655
MaSi <webdeveloper@mateuszsitek.com>
2016-05-15 12:14:14 +0200

    Update copyright year to 2016
    Resolves: #3

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -4,7 +4,7 @@
  * AistInsight (http://mateuszsitek.com/projects/aist-insight)
  *
  * @link      http://github.com/ma-si/aist-insight for the canonical source repository
- * @copyright Copyright (c) 2006-2015 Aist Internet Technologies (http://aist.pl) All rights reserved.
+ * @copyright Copyright (c) 2006-2016 Aist Internet Technologies (http://aist.pl) All rights reserved.
  * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
  */
 
